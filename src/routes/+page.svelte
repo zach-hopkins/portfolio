c4b8f7198329e79a0b77aff0e7fba13141a329e4
Zach <zh93@nau.edu>
2022-08-22 17:42:03 -0700

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,6 @@
 </script>
 
 <main class="main">
-	<link
-		href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap"
-		rel="stylesheet"
-	/>
 	<a id="about" class="about">
 		<div class="container">
 			<img class="headshot" src={headshot} alt="headshot" />
